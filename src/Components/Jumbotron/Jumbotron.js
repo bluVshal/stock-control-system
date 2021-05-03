30ed00abb9b28dc69851f0050a4539450ad2abb5
Pravishal Ishri <pravishal@Pravishals-MBP.local>
2021-05-03 18:16:46 +0400

    Added implementation of Menu Bar

--- src/Components/Jumbotron/Jumbotron.js
+++ src/Components/Jumbotron/Jumbotron.js
@@ -6,7 +6,11 @@ import './Jumbotron.css';
 const Jumbotron = () =>{
    return (
         <Container className="jumbo-container">
-            <Image className="jumbo-back" src="https://en.freejpg.com.ar/image-900/84/849a/F100022535-desert_dunes_without_people.jpg"/>
+            <Image
+                alt="Jumbotron Header"
+                className="jumbo-back"
+                src="https://en.freejpg.com.ar/image-900/84/849a/F100022535-desert_dunes_without_people.jpg"
+            />
             <p className="jumbo-header jumbo-container-text">Stock Control System</p>
         </Container>
     )
--- src/Components/MenuBar/MenuBar.js
+++ src/Components/MenuBar/MenuBar.js
@@ -1,11 +1,31 @@
 import React from 'react';
+import { Container, Menu } from 'semantic-ui-react';
 
+import './MenuBar.css';
 
 const MenuBar = () => {
     return (
-    <div>
-        <p>Menu Bar</p>
-    </div>);
+    <Container className="menu-container">
+        <Menu className="menu-item-container">
+            <Menu.Item
+                name='editorials'
+            >
+                <p className="menu-item">Editorials</p>
+            </Menu.Item>
+
+            <Menu.Item
+                name='reviews'
+            >
+                 <p className="menu-item">Reviews</p>
+            </Menu.Item>
+
+            <Menu.Item
+                name='upcomingEvents'
+            >
+                <p className="menu-item">Upcoming Events</p>
+            </Menu.Item>
+        </Menu>
+    </Container>);
 };
 
 export default MenuBar;
