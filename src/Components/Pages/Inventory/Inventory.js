745a6b91475b2d43e192cc31ff24a647fa30fd1d
BluVshal <blu.vishal@gmail.com>
2021-05-13 14:10:15 +0400

    Implemented the fetch for products in Inventory

--- src/Components/Pages/Inventory/Inventory.js
+++ src/Components/Pages/Inventory/Inventory.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import { Container, Divider} from 'semantic-ui-react';
+import { Container, Divider, Table} from 'semantic-ui-react';
+
+import './Inventory.css';
 
 class Inventory extends React.Component{
+    state={
+        dataDisplay: []
+    };
+
+    async componentDidMount(){
+        await fetch('https://fakestoreapi.com/products')
+        .then(res=>res.json())
+        .then(json=>this.setState({ dataDisplay: json }))
+        .catch((err) => console.error(err)) ;
+    }
+
     render(){
         return(
-            <Container>
+            <Container className="sales-container">
                 <Divider horizontal>Inventory</Divider>
+                <Table basic='very' celled collapsing>
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.HeaderCell>Id</Table.HeaderCell>
+                            <Table.HeaderCell>Name</Table.HeaderCell>
+                            <Table.HeaderCell>Price</Table.HeaderCell>
+                            <Table.HeaderCell>Description</Table.HeaderCell>
+                            <Table.HeaderCell>Category</Table.HeaderCell>
+                            <Table.HeaderCell>Image</Table.HeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+                </Table>
             </Container>
         );
     }
--- src/Components/Pages/Sales/Sales.js
+++ src/Components/Pages/Sales/Sales.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 class Sales extends React.Component{
+
     render(){
         return(
             <div><p>Sales</p></div>
